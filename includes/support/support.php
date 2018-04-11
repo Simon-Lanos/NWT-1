c91b636571da926bf68969d8a137a0e8d38571a5
andreas <andreas.arnolfo@gmail.com>
2018-04-11 16:52:55 +0200

    support modifier

--- includes/support/support.php
+++ includes/support/support.php
@@ -1,27 +1,26 @@
 <html>
 <head>
-
-    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
-    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
-    <title>F.A.Q NWT</title>
-    <link rel="stylesheet" href="support.css" />
+    <title>FAQ</title>
+    <link rel="stylesheet" href="support.css">
 </head>
+
 <body>
-    <div id="mainwrapper">
-        <h1>Formulaire des questions</h1>
-        <div id="content">
-            <h2>Objet de la demande</h2>
-                <input class="objet" type="text" name="Objet de la ou les questions" required placeholder="">
-            <h2>Votre adresse mail</h2>
-                <input class="mail" type="email" name="Mettre l'adresse mail" required placeholder="votre email">
-            <h2>Message</h2>
-                <textarea name="message" rows=4 cols=40></textarea>
-            <div>
-                <input name="recover-submit" class="btn btn-lg btn-warning" value="Envoyer" onclick="location.href='http://localhost:8888/NWT/includes/log/connection.php';" type=submit">
-            </div>
+<form id="contact">
+    <div class="container">
+        <div class="head">
+            <h2>Say Hello</h2>
         </div>
+        <input type="text" name="name" placeholder="Name" /><br />
+        <input  type="email" name="email" placeholder="Email" /><br />
+        <textarea type="text" name="message" placeholder="Message"></textarea><br />
+        <div class="message">Message Sent</div>
+        <button id="submit" type="submit">
+            Send!
+        </button>
     </div>
+</form>
+
+<script src="support.js"></script>
 </body>
+
 </html>
