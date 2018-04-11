7c2aa0ece6fdeb0fed533a21b62a42496fedefbd
andreas <andreas.arnolfo@gmail.com>
2018-04-11 09:47:11 +0200

    login initial

---
+++ includes/Connection/Connection.php
@@ -0,0 +1,65 @@
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Connection</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+    <link href='https://fonts.googleapis.com/css?family=Roboto:400,300,100' rel='stylesheet' type='text/css'>
+
+
+    <link rel="stylesheet" href="connect.css">
+
+
+</head>
+
+<body>
+
+
+<div class="container">
+    <div class="backbox">
+        <div class="loginMsg">
+            <div class="textcontent">
+                <p class="title">Vous n'avez pas de compte ?</p>
+                <p>inscris toi !</p>
+                <button id="switch1">inscription</button>
+            </div>
+        </div>
+        <div class="signupMsg visibility">
+            <div class="textcontent">
+                <p class="title">Deja un compte ?</p>
+                <p>connecte toi !</p>
+                <button id="switch2">connection</button>
+            </div>
+        </div>
+    </div>
+
+    <div class="frontbox">
+        <div class="login">
+            <h2>Connection</h2>
+            <div class="inputbox">
+                <input type="text" name="email" placeholder="  EMAIL">
+                <input type="password" name="password" placeholder="  Mot de passe">
+            </div>
+            <p>Mot de passe oublier ?</p>
+            <button>Connection</button>
+        </div>
+
+        <div class="signup hide">
+            <h2>Inscription</h2>
+            <div class="inputbox">
+                <input type="text" name="fullname" placeholder="  Nom & Prénom">
+                <input type="text" name="email" placeholder="  EMAIL">
+                <input type="password" name="password" placeholder="  Mot de passe">
+            </div>
+            <button>Connection</button>
+        </div>
+
+    </div>
+</div>
+
+<script  src="connect.js"></script>
+
+</body>
+
+</html>
