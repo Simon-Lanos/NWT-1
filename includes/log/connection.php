2f812c39212252555f0e4f2b39f17bfe07aba4d5
Edwin LIENARD <edwin.lienard@outlook.fr>
2018-04-11 11:43:48 +0200

    ajout css select

--- includes/log/connection.php
+++ includes/log/connection.php
@@ -6,15 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
     <link href='https://fonts.googleapis.com/css?family=Roboto:400,300,100' rel='stylesheet' type='text/css'>
-
-
     <link rel="stylesheet" href="connect.css">
-
-
 </head>
-
 <body>
-
 <div class="container">
     <div class="backbox">
         <div class="loginMsg">
@@ -32,18 +26,18 @@
             </div>
         </div>
     </div>
-
     <div class="frontbox">
         <div class="login">
             <h2>Connection</h2>
             <div class="inputbox">
-                <input type="text" name="email" placeholder="  EMAIL">
-                <input type="password" name="password" placeholder="  Mot de passe">
+                <form>
+                    <input type="text" name="email" placeholder="  EMAIL">
+                    <input type="password" name="password" placeholder="  Mot de passe">
+                </form>
             </div>
             <p>Mot de passe oublier ?</p>
             <button>Connection</button>
         </div>
-
         <div class="signup hide">
             <h2>Inscription</h2>
             <div class="inputbox">
@@ -51,25 +45,20 @@
                     <input type="text" name="fullname" placeholder="  Nom & Prénom">
                     <input type="text" name="email" placeholder="  EMAIL">
                     <input type="password" name="password" placeholder="  Mot de passe">
-
-                    <select name ="specialite" id="specialite">
-                        <option value="develop" selected>Dévelopement</option>
-                        <option value="develop">Marketing</option>
-                        <option value="develop">Web-design</option>
-                        <option value="develop">Intervenant</option>
-                    </select>
+                    <div class="positionSelect">
+                        <select name ="specialite" id="specialite">
+                            <option value="develop" selected>Dévelopement</option>
+                            <option value="develop">Marketing</option>
+                            <option value="develop">Web-design</option>
+                            <option value="develop">Intervenant</option>
+                        </select>
+                    </div>
                 </form>
-
             </div>
-                <button>Connection</button>
+                <button>Inscription</button>
         </div>
-
     </div>
-
 </div>
-
 <script  src="connect.js"></script>
-
 </body>
-
 </html>
