4a0923f0f14a5382229c17b77496f0791dfa8559
andreas <andreas.arnolfo@gmail.com>
2018-04-11 15:56:46 +0200

    modification du bouton envoyer du F.A.Q

--- includes/support/support.php
+++ includes/support/support.php
@@ -1,6 +1,11 @@
 <html>
 <head>
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+    <!------ Include the above in your HEAD tag ---------->
 
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
     <title>F.A.Q NWT</title>
 
     <link rel="stylesheet" href="style.css" />
@@ -41,7 +46,8 @@
 
 <div>
 
-    <input class="buttonSend" type="submit" name="envoyer" value="Soumettre">
+    <input name="recover-submit" class="btn btn-lg btn-warning" value="Envoyer" onclick="location.href='http://localhost:8888/NWT/includes/log/connection.php';" type=submit">
+
 
 </div>
 
