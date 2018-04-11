7a0294d2a3ef391b244c4ffbf97258f5d0616028
Edwin LIENARD <edwin.lienard@outlook.fr>
2018-04-11 15:57:46 +0200

    modification support

--- includes/support/support.php
+++ includes/support/support.php
@@ -1,50 +1,30 @@
 <html>
 <head>
-
     <title>F.A.Q NWT</title>
-
-    <link rel="stylesheet" href="style.css" />
-
+    <link rel="stylesheet" href="support.css" />
 </head>
-
 <body>
-
-<h1>Formulaire des questions</h1>
-
-<h2>Category</h2>
-
-<select class="category">
-
-    <option value="Category 1">Categorie 1</option>
-
-    <option value="Category 2">Categorie 2</option>
-
-    <option value="Category 3">Categorie 3</option>
-
-    <option value="Category 4">Categorie 4</option>
-
-    <option value="Category 5">Categorie 5</option>
-
-</select>
-
-<h2>Objet de la demande</h2>
-
-<input class="objet" type="text" name="Objet de la ou les questions" required="">
-
-<h2>Votre adresse mail</h2>
-
-<input class="mail" type="email" name="Mettre l'adresse mail" required="">
-
-<h2>Message</h2>
-
-<textarea name="message" rows=4 cols=40></textarea>
-
-<div>
-
-    <input class="buttonSend" type="submit" name="envoyer" value="Soumettre">
-
-</div>
-
+    <div id="mainwrapper">
+            <h1>Support</h1>
+            <h2>Catégories</h2>
+        <div id="content">
+            <select class="category">
+                <option value="Category 1">Categorie 1</option>
+                <option value="Category 2">Categorie 2</option>
+                <option value="Category 3">Categorie 3</option>
+                <option value="Category 4">Categorie 4</option>
+                <option value="Category 5">Categorie 5</option>
+            </select>
+            <h2>Objet de la demande</h2>
+            <input class="objet" type="text" name="Objet de la ou les questions" required="">
+            <h2>Votre adresse mail</h2>
+            <input class="mail" type="email" name="Mettre l'adresse mail" required="">
+            <h2>Message</h2>
+            <textarea name="message" rows=4 cols=40></textarea>
+            <div>
+                <input class="buttonSend" type="submit" name="envoyer" value="Soumettre">
+            </div>
+        </div>
+    </div>
 </body>
-
 </html>
