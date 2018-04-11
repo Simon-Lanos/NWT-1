199ea3c2e63bdf972b5ec7a2b5325983817edb19
Marc-Alban <marcalban@live.fr>
2018-04-11 11:18:45 +0200

    Ajout Insertion, delete, update

--- classes/PDO.php
+++ classes/PDO.php
@@ -8,19 +8,81 @@
 
 class PDO
 {
-    private $user = "root";
-    private $pass = "";
 
-    public function __construct()
-    {
+    const DSN = 'mysql:host=localhost;dbname=tuto';
+    const USER = 'root';
+    const PASSWORD = '';
 
+    public function __construct() {
         try {
-        $bdd = new PDO( 'mysql:host=localhost;dbname=tuto', $this->user,$this->pass );
+            parent::__construct( self::DSN, self::USER, self::PASSWORD );
         }
         catch ( PDOException $e ) {
-        print( 'Erreur : ' . $e->getMessage() );
+            die( 'Erreur : ' . $e->getMessage() );
         }
-
     }
 
+    static public function select ( string $query, array $params = [] ) : array {
+        $bdd = new DB;
+
+        if ( $params ) {
+            $req = $bdd->prepare( $query );
+            $req->execute( $params );
+        }
+        else {
+            $req = $bdd->query( $query );
+        }
+
+        $data = $req->fetchAll();
+
+        return $data;
+    }
+
+    static public function update ( string $query, array $params = [] ) : int {
+        $bdd = new DB;
+
+        if ( $params ) {
+            $req = $bdd->prepare( $query );
+            $req->execute( $params );
+        }
+        else {
+            $req = $bdd->query( $query );
+        }
+
+        $updated = $req->rowCount();
+
+        return $updated;
+    }
+
+    static public function insert ( string $query, array $params = [] ) : int {
+        $bdd = new DB;
+
+        if ( $params ) {
+            $req = $bdd->prepare( $query );
+            $req->execute( $params );
+        }
+        else {
+            $req = $bdd->query( $query );
+        }
+
+        $inserted = $req->rowCount();
+
+        return $inserted;
+    }
+
+    static public function delete ( string $query, array $params = [] ) : int {
+        $bdd = new DB;
+
+        if ( $params ) {
+            $req = $bdd->prepare( $query );
+            $req->execute( $params );
+        }
+        else {
+            $req = $bdd->query( $query );
+        }
+
+        $deleted = $req->rowCount();
+
+        return $deleted;
+    }
 }
