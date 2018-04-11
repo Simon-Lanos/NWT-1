401b4e180573dc9861a60eaff400a0d53c327436
Edwin LIENARD <edwin.lienard@outlook.fr>
2018-04-11 17:24:40 +0200

    modification support

--- includes/support/support.php
+++ includes/support/support.php
@@ -3,24 +3,21 @@
     <title>FAQ</title>
     <link rel="stylesheet" href="support.css">
 </head>
-
 <body>
 <form id="contact">
     <div class="container">
         <div class="head">
-            <h2>Say Hello</h2>
+            <h2>Un Problème ?</h2>
         </div>
         <input type="text" name="name" placeholder="Name" /><br />
         <input  type="email" name="email" placeholder="Email" /><br />
+        <input type="text" name="objet" placeholder="Objet de votre demande" /><br />
         <textarea type="text" name="message" placeholder="Message"></textarea><br />
         <div class="message">Message Sent</div>
-        <button id="submit" type="submit">
-            Send!
-        </button>
+        <button id="submit" type="submit">Send!</button>
+        <input name="recover-submit" class="button" value="retour" onclick="location.href='http://localhost:8888/NWT/includes/log/connection.php';" type="submit">
     </div>
 </form>
-
 <script src="support.js"></script>
 </body>
-
 </html>
