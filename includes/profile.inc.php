75a3b1b091cc3c098e20aad86ed54a068e912c54
DonCuicui <alexanrre.quilan@gmail.com>
2018-04-11 12:00:56 +0200

    Html profile

---
+++ includes/profile.inc.php
@@ -0,0 +1,17 @@
+?>
+<div class="">
+    <a class="noUnderLine" href="https://www.google.fr"><div class="demiCercle"><div class="croix1"></div><div class="croix2"></div></div></a>
+    <div class="profilContainer">
+        <img class="profilPicture" src="https://scontent-mrs1-1.xx.fbcdn.net/v/t1.0-0/p206x206/29433352_1755952454463031_6669985011330065173_n.jpg?_nc_cat=0&oh=8fe281522adf2430974c3ccbfe0e5ef9&oe=5B2E2E55">
+        <div class="noColumn">
+            <p class="lobster">Alexandre </p>
+            <p class="lobster marginLeft2"> Delaistre</p>
+        </div>
+        <p class="lobster">Groupe: Developpeur</p>
+        <a class="noUnderLine" href="https://www.google.fr"><div class="button lobster">Mes Prochains Cours</div></a>
+        <a class="noUnderLine" href="https://www.google.fr"><div class="buttonJaune lobster marginTopDix ">Signaler une Absence</div></a>
+        <a class="noUnderLine" href="https://www.google.fr"><div class="buttonNoir lobster marginTopDix ">Se deconecter</div></a>
+
+    </div>
+</div>
+<?php
