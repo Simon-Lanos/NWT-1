214975a2f2d0df74b71b483bd04a7db806b766e7
Marc-Alban <millet.marcalban@gmail.com>
2018-04-13 15:57:02 +0200

    Modif en cours v5

---
+++ includes/log/enregistrement.php
@@ -0,0 +1,39 @@
+<html>
+<head>
+    <!--salut-->
+    <meta charset="UTF-8">
+    <title>Connection</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+    <link href='https://fonts.googleapis.com/css?family=Roboto:400,300,100' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="connect.css">
+</head>
+<body>
+
+<div class="container">
+    <div class="backbox">
+        <div class="loginMsg">
+            <div class="textcontent">
+                <p class="title">Vous n'avez pas de compte ?</p>
+                <p>inscris toi !</p>
+                <button id="switch1">inscription</button>
+            </div>
+        </div>
+        <div class="signupMsg visibility">
+            <div class="textcontent">
+                <p class="title">Déjà un compte ?</p>
+                <p>connecte toi !</p>
+                <button id="switch2">connexion</button>
+            </div>
+        </div>
+    </div>
+    <div class="frontbox">
+<?php include 'connection.php'?>
+<?php include 'inscription.php'?>
+
+    </div>
+</div>
+
+<script  src="connect.js"></script>
+</body>
+</html>
