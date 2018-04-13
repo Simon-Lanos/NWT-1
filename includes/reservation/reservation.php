e658cacd652e18bbf78ec08349a9ee9475ccd344
Rodéric Camesella <roderic.camesella@viacesi.fr>
2018-04-13 09:01:22 +0200

    ajout d'une page de résa avec des couleurs temporaires ( attention présence d'un reset css dans le fichier reservation.css)

---
+++ includes/reservation/reservation.php
@@ -0,0 +1,73 @@
+<html lang="fr">
+
+<head>
+
+    <title>Réservation</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="reservation.css" />
+
+</head>
+
+<body>
+
+<table>
+
+    <thead>
+
+    <tr class="ligneJour">
+
+        <td class="none"></td>
+        <td class="nope"></td>
+        <td class="jourSemaine">Lundi</td>
+        <td class="jourSemaine">Mardi</td>
+        <td class="jourSemaine">Mercredi</td>
+        <td class="jourSemaine">Jeudi</td>
+        <td class="jourSemaine">Vendredi</td>
+        <td class="jourSemaine">Samedi</td>
+        <td class="jourSemaine">Dimanche</td>
+
+    </tr>
+
+    </thead>
+
+    <tr>
+
+        <td class="triche"></td>
+        <td class="reserv">Matin</td>
+        <td class="grille"></td>
+        <td class="grille"></td>
+        <td class="grille"></td>
+        <td class="grille"></td>
+        <td class="grille"></td>
+        <td class="grille"></td>
+        <td class="grille"></td>
+
+    </tr>
+    <tr>
+        <td rowspan="2" class="salle"><p>Salle 1</p></td>
+        <td class="reserv">Après-midi</td>
+        <td class="grille"></td>
+        <td class="grille"></td>
+        <td class="grille"></td>
+        <td class="grille"></td>
+        <td class="grille"></td>
+        <td class="grille"></td>
+        <td class="grille"></td>
+    </tr>
+    <tr>
+        <td class="reserv">Journée entière</td>
+        <td class="grille"></td>
+        <td class="grille"></td>
+        <td class="grille"></td>
+        <td class="grille"></td>
+        <td class="grille"></td>
+        <td class="grille"></td>
+        <td class="grille"></td>
+    </tr>
+
+</table>
+
+
+</body>
+
+</html>
